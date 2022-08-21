413ad788aa3d1628a03126798b8ad9a3d3a577e4
사공지은 <gonii0821@naver.com>
2022-08-22 02:40:44 +0900

    2022-08-22

--- algoPractice/src/BOJ11053.java
+++ algoPractice/src/BOJ11053.java
@@ -6,27 +6,21 @@ import java.util.StringTokenizer;
 // 가장 긴 증가하는 부분 수열
 
 public class BOJ11053 {
-	
-	static int N;
-	static int[] nums;
-	static int[] DP;
-	static int answer;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 
-		nums = new int[N];
-		DP = new int[N];
+		int[] nums = new int[N];
+		int[] DP = new int[N];
 		
 		StringTokenizer stk = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			nums[i] = Integer.parseInt(stk.nextToken());
+			DP[i] = 1; // 모든 수열의 수열 개수를 1로 세팅
 		}
 		
 		for (int cur = 0; cur < N; cur++) {
-			DP[cur] = 1; // 현재 인덱스의 수열 개수 1로 세팅
-			
 			// 현재 이전의 원소들에 대해
 			for (int prev = 0; prev < cur; prev++) {
 				// 현재의 수가 이전의 수보다 크고
@@ -37,6 +31,8 @@ public class BOJ11053 {
 				}
 			}
 		}
+		
+		int answer = 0;
 		
 		for (int i = 0; i < N; i++) {
 			answer = Math.max(answer, DP[i]);
