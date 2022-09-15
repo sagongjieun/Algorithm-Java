fdee8b237b247e548e96f73ae25bb979ec81a16e
사공지은 <gonii0821@naver.com>
2022-09-15 15:34:34 +0900

    2022-09-15

---
+++ algoPractice/src/BOJ8370.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+// Plane
+
+public class BOJ8370 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print((sc.nextInt() * sc.nextInt()) + (sc.nextInt() * sc.nextInt()));
+	}
+
+}
